c5b90f765433da55d864d0014846aaaf03222746
Gustavo Santana <gustavosantanafue@gmail.com>
2024-07-09 16:27:44 -0400

    Changes

--- Practica examen/funcionesmain.py
+++ Practica examen/funcionesmain.py
@@ -1,3 +1,17 @@
 import time, datetime, csv
+#Credenciales de login
+logincredentials={"Gustavo": "G234",
+                  "Javier":"12345",
+                  "admin":"admin",
+                  "a":"a"}
+#Funcion para Login  
+def login(inputusuario, inputcontraseña):
+     #Si estan correctas las credenciales devuelve True
+     if inputusuario in logincredentials and logincredentials[inputusuario]==inputcontraseña:
+          return True
+     else:
+     #Si no devuelve False
+          return False
+#Funcion para mostrar el menu al usuario
 def menu():
      print("==========================\nMenu\n==========================\n1.- Agregar producto\n2.- Modificar producto\n3.- Quitar producto\n4.- Guardar inventario en excel\n5.- Salir del programa\n")
--- Practica examen/main.py
+++ Practica examen/main.py
@@ -1,5 +1,32 @@
+#Funciones
 import funcionesmain as func
+#Opciones para los menus
 opcion_main_menu=0
 opcion_add=0
 opcion_modify=0
 opcion_remove=0
+#Empieza el programa
+while func.login==False:
+    print("Bienvenid@!")
+    inputusuario=input("Ingrese su usuario: ")
+    inputcontraseña=input("Ingrese su contraseña: ")
+    func.login(inputusuario,inputcontraseña)
+while opcion_main_menu!=5:
+    func.menu()
+    try:
+        opcion_main_menu=int(input(""))
+    except:
+        print("\nIngrese una opcion valida.")
+    else:
+        if opcion_main_menu==1:
+            print()
+        elif opcion_main_menu==2:
+            print()
+        elif opcion_main_menu==3:
+            print()
+        elif opcion_main_menu==4:
+            print()
+        elif opcion_main_menu==5:
+            print()
+        else:
+            print("\nIngrese una opcion valida.")
